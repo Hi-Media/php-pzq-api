4687dac9899de981eb9e45d7a2ae4ff7bf046d93
Chabot Vincent <vince.chabot@gmail.com>
2014-07-15 16:46:04 +0200

    Update Consumer.php to add License

--- src/PZQ/Consumer.php
+++ src/PZQ/Consumer.php
@@ -2,6 +2,14 @@
 
 namespace HIM\PZQ;
 
+/**
+ * 
+ * Copyright (c) 2014 Vincent Chabot <vchabot@hi-media.com>
+ * Licensed under the GNU Lesser General Public License v3 (LGPL version 3).
+ *
+ * @copyright 2014 Vincent Chabot <vchabot@hi-media.com>
+ * @license http://www.gnu.org/licenses/lgpl.html
+ */
 class Consumer
 {
     private $socket;
